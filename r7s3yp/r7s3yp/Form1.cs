fbb209ee1b93f23ddd4d7b08365cab54508c9e52
Fruzsina Bomba <fruzsina.bomba@stud.uni-corvinus.hu>
2022-10-07 19:43:44 +0200

    Negyedik

---
+++ r7s3yp/r7s3yp/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace r7s3yp
+{
+    public partial class Form1 : Form
+    {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
+        }
+    }
+}

fe34e8e2ecd98a5b438533c9d1892630a1334d94
r7s3yp <r7s3yp@SZA0032056>
2022-10-21 12:47:51 +0200

    :)

--- r7s3yp_6/r7s3yp_6/Form1.cs
+++ r7s3yp_6/r7s3yp_6/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace r7s3yp_6
@@ -16,11 +17,24 @@ namespace r7s3yp_6
     public partial class Form1 : Form
     {
         BindingList<RateData> rates = new BindingList<RateData>();
+        BindingList<string> Currencies = new BindingList<string>();
         public Form1()
         {
             InitializeComponent();
             KulonFuggveny();
+
+
+            
+            KulonFuggveny();
+            RefreshData();
             dataGridView1.DataSource = rates;
+            comboBox1.DataSource = Currencies;
+        }
+
+        private void RefreshData()
+        {
+            
+            rates.Clear();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -55,16 +69,54 @@ namespace r7s3yp_6
                
                 rate.Date = DateTime.Parse(element.GetAttribute("date"));
 
-                // Valuta
+    
                 var childElement = (XmlElement)element.ChildNodes[0];
                 rate.Currency = childElement.GetAttribute("curr");
 
-                // Érték
+            
                 var unit = decimal.Parse(childElement.GetAttribute("unit"));
                 var value = decimal.Parse(childElement.InnerText);
                 if (unit != 0)
                     rate.Value = value / unit;
+
+                chartRateData.DataSource = rates;
+
+                var series = chartRateData.Series[0];
+                series.ChartType = SeriesChartType.Line;
+                series.XValueMember = "Date";
+                series.YValueMembers = "Value";
+                series.BorderWidth = 2;
+
+                var legend = chartRateData.Legends[0];
+                legend.Enabled = false;
+
+                var chartArea = chartRateData.ChartAreas[0];
+                chartArea.AxisX.MajorGrid.Enabled = false;
+                chartArea.AxisY.MajorGrid.Enabled = false;
+                chartArea.AxisY.IsStartedFromZero = false;
             }
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
 
         }
     }

b0fe86348b71bc289b02d020ef2d9ac32291453d
r7s3yp <r7s3yp@SZA0032056>
2022-10-21 12:31:21 +0200

    1-4 feladatok

---
+++ r7s3yp_6/r7s3yp_6/Entities/RateData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace r7s3yp_6.Entities
+{
+    class RateData
+    {
+
+        public DateTime Date { get; set; }
+        public string Currency { get; set; }
+        public decimal Value { get; set; }
+    }
+}

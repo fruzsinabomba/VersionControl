85d322526b5ff3fae6681697b89868318cfdd239
r7s3yp <r7s3yp@SZA0032056>
2022-11-04 12:38:43 +0100

    Labda létrehozása

---
+++ WindowsFormsApp1/WindowsFormsApp1/BallFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.Entities;
+
+namespace WindowsFormsApp1
+{
+    public class BallFactory
+    {
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
